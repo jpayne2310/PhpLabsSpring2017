e444d15b298f947acaa9c885087a55525142befe
James Payne <jrpayne@email.neit.edu>
2017-05-01 17:20:40 -0400

    started lab6

---
+++ lab6/js/app.js
@@ -0,0 +1,22 @@
+//function wrapper use JS angular files instead of global variables
+(function() {
+    'use strict';
+    angular
+            .module('app', ['ngRoute'])
+            .config(config);
+    
+    config.$inject = ['$routeProvider'];
+    
+    function config($routeProvider) {
+            $routeProvider.
+                when('/', {
+                    templateUrl: 'js/phone-list.template.html',
+                    controller: 'PhoneListController',
+                    controllerAs: 'vm'
+            }).
+                otherwise({
+                    redirectTo: '/'
+            });
+        }
+        
+})();
